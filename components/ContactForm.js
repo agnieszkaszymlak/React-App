07447ea0f9eebb246fb886a3d433cfe0d0232d34
Agnieszka Szymlak <agniepiech@gmail.com>
2019-09-21 22:33:46 +0200

    Change ContactForm

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -8,7 +8,7 @@ var ContactForm = React.createClass({
         <form className={'contactForm'}>
             <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}/>
             <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}/>
-            <input type={'text'} placeholder={'Email'} value={this.props.contact.email}>/>
+            <input type={'text'} placeholder={'Email'} value={this.props.contact.email}/>
             <button type={'submit'}>Dodaj kontakt</button>
         </form>
       )
